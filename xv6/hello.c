33f55c02f03dbdafcd875a8a07506270d39ef197
mm7259 <mm7259@nyu.edu>
2019-03-29 14:09:14 +0000

    testing macros

--- xv6/hello.c
+++ xv6/hello.c
@@ -6,6 +6,13 @@
 int 
 main(void){ 
 	printf(1, "hello world\n"); 
+	#ifdef DEFAULT 
+	printf(1, "this is how macros work\n"); 
+	#endif 
+	#ifdef OTHERMACRO
+	printf(1, "other macro\n");
+	#endif
 	exit();
+
 }
  
